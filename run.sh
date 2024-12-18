5e4f815116d6bdd86135165b5e3e24ed2763090f
Bram Otte <bram@otte.biz>
2024-12-18 11:46:43 +0100

    Output strategy for max reroll and extended model

--- run.sh
+++ run.sh
@@ -4,25 +4,25 @@ defense=6
 attack=6
 folder=out/strats/$defense-$attack
 
-mkdir -p out/models
 mkdir -p $folder
+mkdir -p $folder/extended
 
-cat models/battles.pm models/battles-wounds.pm > out/models/battles-wounds.pm
-cat models/battles.pm models/battles-reroll.pm > out/models/battles-reroll.pm
+prism models/extended.pm models/extended.pctl -prop 1 -cuddmaxmem 16g -javamaxmem 16g -exportstrat $folder/extended/pmax1.tra:type=induced,mode=reduce
+# prism models/extended.pm -exportmodel $folder/extended/model.all -cuddmaxmem 16g -javamaxmem 16g
 
 
-prism models/battles.pm -const defense=$defense,attack=$attack -exportstates $folder/states.sta -exportlabels $folder/labels.lab
-prism out/models/battles-wounds.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/rmax.tra:type=induced,mode=reduce  -prop 1  -cuddmaxmem 2g -javamaxmem 2g
-prism out/models/battles-reroll.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/rmax.tra:type=induced,mode=reduce  -prop 1  -cuddmaxmem 2g -javamaxmem 2g
 
-prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax1.tra:type=induced,mode=reduce -prop 2  -cuddmaxmem 2g -javamaxmem 2g
-prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax2.tra:type=induced,mode=reduce -prop 3  -cuddmaxmem 2g -javamaxmem 2g
-prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax3.tra:type=induced,mode=reduce -prop 4  -cuddmaxmem 2g -javamaxmem 2g
-prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax4.tra:type=induced,mode=reduce -prop 5  -cuddmaxmem 2g -javamaxmem 2g
-prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax5.tra:type=induced,mode=reduce -prop 6  -cuddmaxmem 2g -javamaxmem 2g
-prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax6.tra:type=induced,mode=reduce -prop 7  -cuddmaxmem 2g -javamaxmem 2g
-prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax7.tra:type=induced,mode=reduce -prop 8  -cuddmaxmem 2g -javamaxmem 2g
-prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax8.tra:type=induced,mode=reduce -prop 9  -cuddmaxmem 2g -javamaxmem 2g
-prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax9.tra:type=induced,mode=reduce -prop 10 -cuddmaxmem 2g -javamaxmem 2g
-prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax10.tra:type=induced,mode=reduce -prop 11 -cuddmaxmem 2g -javamaxmem 2g
-prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/maxreroll.tra:type=induced,mode=reduce -prop 12 -cuddmaxmem 2g -javamaxmem 2g
+prism models/battles.pm -const defense=$defense,attack=$attack -exportstates $folder/states.sta -exportlabels $folder/labels.lab -nofixdl
+prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/rmax.tra:type=induced,mode=reduce  -prop 1  -cuddmaxmem 2g -javamaxmem 2g -nofixdl -exportresults $folder/results.txt
+prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/rmax-reroll.tra:type=induced,mode=reduce  -prop 12  -cuddmaxmem 2g -javamaxmem 2g -nofixdl
+
+prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax1.tra:type=induced,mode=reduce -prop 2  -cuddmaxmem 2g -javamaxmem 2g -nofixdl
+prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax2.tra:type=induced,mode=reduce -prop 3  -cuddmaxmem 2g -javamaxmem 2g -nofixdl
+prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax3.tra:type=induced,mode=reduce -prop 4  -cuddmaxmem 2g -javamaxmem 2g -nofixdl
+prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax4.tra:type=induced,mode=reduce -prop 5  -cuddmaxmem 2g -javamaxmem 2g -nofixdl
+prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax5.tra:type=induced,mode=reduce -prop 6  -cuddmaxmem 2g -javamaxmem 2g -nofixdl
+prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax6.tra:type=induced,mode=reduce -prop 7  -cuddmaxmem 2g -javamaxmem 2g -nofixdl
+prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax7.tra:type=induced,mode=reduce -prop 8  -cuddmaxmem 2g -javamaxmem 2g -nofixdl
+prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax8.tra:type=induced,mode=reduce -prop 9  -cuddmaxmem 2g -javamaxmem 2g -nofixdl
+prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax9.tra:type=induced,mode=reduce -prop 10 -cuddmaxmem 2g -javamaxmem 2g -nofixdl
+prism models/battles.pm models/battles.pctl -const defense=$defense,attack=$attack -exportstrat $folder/pmax10.tra:type=induced,mode=reduce -prop 11 -cuddmaxmem 2g -javamaxmem 2g -nofixdl
